9cea8abd71ce2402e67a3da7f2f5a629625e9043
Joshua Wehner <joshua.wehner@github.com>
2015-05-28 16:48:07 -0500

    Almost there: Only owners can use this filter

--- 2fa-audit.go
+++ 2fa-audit.go
@@ -2,8 +2,47 @@ package main
 
 import (
   "fmt"
+  "os"
+  "github.com/octokit/go-octokit/octokit"
 )
 
+type Member struct {
+  Login string `json:"login"`
+}
+
 func main() {
-  fmt.Println("Usage: given an org, print list of users *without* 2fa enabled")
+  if len(os.Args[1:]) < 1 {
+    fmt.Println("Usage: given an org, print list of users *without* 2fa enabled")
+    return
+  }
+
+  orgName := os.Args[1]
+  fmt.Printf("Org Name is: %v", orgName)
+
+  memberURL := octokit.Hyperlink("/orgs/{org}/members{?filter}") // type,page,per_page,sort
+  url, err := memberURL.Expand(octokit.M{"org": orgName, "filter": "2fa_disabled"})
+
+  fmt.Println("What is URL: " + url.String())
+
+  if err != nil {
+    fmt.Println("There was an error: ", err)
+    return
+  }
+  fmt.Printf("URL is: %v\n", url)
+
+  client := octokit.NewClient(nil)
+  req, _ := client.NewRequest(url.String())
+  var members []Member
+  // result := client.get(url, &members)
+  req.Get(&members)
+
+  // if result.HasError() {
+  //   fmt.Println("Error: ", err)
+  //   return
+  // }
+
+  if len(members) == 0 { fmt.Println("No users have 2fa disabled.") }
+  for i:=0; i<len(members); i++ {
+    fmt.Println(members[i].Login)
+  }
 }
